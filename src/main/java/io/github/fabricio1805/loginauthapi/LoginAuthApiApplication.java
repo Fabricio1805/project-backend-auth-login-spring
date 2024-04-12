08d0902668e57deab96289a3b3cb45d125d52bde
Fabricio1805 <fabricio.ss09@outlook.com>
2024-04-11 22:01:47 -0300

    feat: initialize project

---
+++ src/main/java/io/github/fabricio1805/loginauthapi/LoginAuthApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.fabricio1805.loginauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthApiApplication.class, args);
+	}
+
+}
