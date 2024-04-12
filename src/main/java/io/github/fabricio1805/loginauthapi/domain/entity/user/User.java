0a5b07bee867c15a972affb199fbb6f01a5e4e18
Fabricio1805 <fabricio.ss09@outlook.com>
2024-04-11 22:25:39 -0300

    feat: add Entity user

---
+++ src/main/java/io/github/fabricio1805/loginauthapi/domain/entity/user/User.java
@@ -0,0 +1,46 @@
+package io.github.fabricio1805.loginauthapi.domain.entity.user;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String password;
+
+  @CreationTimestamp
+  @Column(name = "created_at") 
+  private LocalDateTime createdAt;
+
+
+  @UpdateTimestamp
+  @Column(name = "updated_at") 
+  private LocalDateTime updatedAt;
+}
