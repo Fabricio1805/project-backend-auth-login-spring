ec2793e4a5cbb6750127fbe219ab817946223c2e
Fabricio1805 <fabricio.ss09@outlook.com>
2024-04-15 22:45:30 -0300

    feat: add class securityFilter

--- src/main/java/io/github/fabricio1805/loginauthapi/infrastructure/repository/user/UserRepository.java
+++ src/main/java/io/github/fabricio1805/loginauthapi/infrastructure/repository/user/UserRepository.java
@@ -1,5 +1,7 @@
 package io.github.fabricio1805.loginauthapi.infrastructure.repository.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import io.github.fabricio1805.loginauthapi.domain.entity.user.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-  
+  Optional<User> findByEmail(String email);
 }
